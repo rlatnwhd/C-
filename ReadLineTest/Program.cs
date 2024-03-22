3ca2c80a6a7e3257e26078078def4ff085e43708
김수종 <127273652+rlatnwhd@users.noreply.github.com>
2024-03-22 09:42:27 +0900

    2024-03-21

---
+++ ReadLineTest/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("첫 번째 정수를 입력하세요 : ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("두 번째 정수를 입력하세요 : ");
+        int num2 = int.Parse(Console.ReadLine());
+
+
+        Console.WriteLine(num1 + " + " + num2 + " = " + (num1+num2));
+        Console.WriteLine(num1 + " - " + num2 + " = " + (num1-num2));
+        Console.WriteLine(num1 + " * " + num2 + " = " + (num1*num2));
+        Console.WriteLine(num1 + " / " + num2 + " = " + ((float)num1/num2).ToString("F2"));
+        Console.WriteLine(num1 + " % " + num2 + " = " + (num1%num2));
+
+    }
+}
