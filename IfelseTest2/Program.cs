fc6e1cd3825bd2ef457d91738dd2ddad68fbc78c
김수종 <127273652+rlatnwhd@users.noreply.github.com>
2024-03-22 11:34:34 +0900

    2024-03-22

---
+++ IfelseTest2/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("2 자리 정수 입력(10~99) : ");
+        int num = int.Parse(Console.ReadLine());
+        if (num >= 10 && num <= 99)
+        {
+            if (num % 10 == num / 10)
+            {
+                Console.WriteLine("Yes! 10의 자리와 1의 자리가 같습니다.");
+            }
+            else
+            {
+                Console.WriteLine("No! 10의 자리와 1의 자리가 다릅니다.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("10~99 사이의 정수만 입력하세요.");
+        }
+    }
+}
