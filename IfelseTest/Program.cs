df3d6e9a451f0be0c23587c26436f3e65af413cd
김수종 <127273652+rlatnwhd@users.noreply.github.com>
2024-03-22 11:33:37 +0900

    2024-03-22

---
+++ IfelseTest/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("나이 입력 : ");
+        int age = int.Parse(Console.ReadLine());
+        if (age >= 9)
+        {
+            Console.Write("키 입력 : ");
+            int height = int.Parse(Console.ReadLine());
+            if (height >= 130)
+            {
+                Console.WriteLine("고속 롤러코스터 입장 가능");
+            }
+            else
+            {
+                Console.WriteLine("저속 롤러코스터 입장 가능");
+            }
+        }
+        else
+        {
+            Console.WriteLine("입장 불가!");
+        }
+        
+
+        
+
+    }
+}
