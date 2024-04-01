c336ed5b0a10f60a1fb85b5cf0d5451ab9591474
김수종 <127273652+rlatnwhd@users.noreply.github.com>
2024-04-01 18:02:01 +0900

    2024-03-29

---
+++ MovingAt/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = 1;
+        while (x < 50)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(x, 5);
+
+            // 출력합니다.
+            switch (x % 3)
+            {
+                case 0:
+                    Console.WriteLine("__@");
+                    break;
+                case 1:
+                    Console.WriteLine("_^@");
+                    break;
+                default:
+                    Console.WriteLine("^_@");
+                    break;
+            }
+            Thread.Sleep(500);
+            x++;
+        }
+    }
+}
