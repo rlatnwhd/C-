7f7e7903bbc0d3b59c1afd14838a630eb1ac8312
김수종 <127273652+rlatnwhd@users.noreply.github.com>
2024-04-01 17:57:40 +0900

    2024-03-29

---
+++ ForWhileTest2/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("처음 문자 입력 : ");
+        char start = char.Parse(Console.ReadLine());
+        Console.Write("마지막 문자 입력 : ");
+        char end = char.Parse(Console.ReadLine());
+
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write($"{(char)i, 3}");
+        }
+    }
+}
