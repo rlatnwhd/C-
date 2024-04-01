6e6c3679fbaa9c5796ed38ec99c680eb84f47716
김수종 <127273652+rlatnwhd@users.noreply.github.com>
2024-04-01 17:56:19 +0900

    2024-03-29

---
+++ ForWhileTest1/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int i = 0;
+        int[] intArray = new int[5];
+        while (i < intArray.Length)
+        {
+            Console.Write($"{i+1}번째 입력 : ");
+            int num = int.Parse(Console.ReadLine());
+            intArray[i] = num;
+            i++;
+        }
+        Console.WriteLine();
+        for (i=0; i < intArray.Length; i++)
+        {
+            Console.WriteLine($"{i+1}번째 출력 : {intArray[i]}");
+        }
+    }
+}
