41451e51041eabab21209a9ba0fbb88fc1a057e2
김수종 <127273652+rlatnwhd@users.noreply.github.com>
2024-04-01 16:54:03 +0900

    2024-03-28

---
+++ SwitchCaseTest1/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("점수를 입력하세요 : ");
+        int score = int.Parse(Console.ReadLine());
+
+        switch (score / 10)
+        {
+            case 9:
+                Console.WriteLine("A학점");
+                break;
+            case 8:
+                Console.WriteLine("B학점");
+                break;
+            case 7:
+                Console.WriteLine("C학점");
+                break;
+            case 6:
+                Console.WriteLine("D학점");
+                break;
+            default:
+                Console.WriteLine("재수강으로 분발하세요.");
+                break;
+        }
+    }
+}
