c6535b25ac4f6569429b7d374fc74da828b3b8ce
김수종 <127273652+rlatnwhd@users.noreply.github.com>
2024-04-05 11:21:36 +0900

    2024-04-05

---
+++ RandomClass/ClassTest/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    class Car
+    {
+        int carNumber;
+        public DateTime inTime;  // DateTime 데이터타입
+        public DateTime outTime;
+
+        public void SetInTime()
+        {
+            this.inTime = DateTime.Now;// this -> 이름이 같은 다른 변수와 혼동하지 않도록 정확하게 가르켜 줌
+            Console.WriteLine("출입시간 : " + this.inTime);
+        }
+        public void SetOutTime()
+        {
+            this.outTime = DateTime.Now;
+            Console.WriteLine("출차시간 : " + this.outTime);
+            // Console.WriteLine(inTime.ToString());
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.SetInTime();
+        car.SetOutTime();
+        Console.WriteLine($"\n출입시간 : {car.inTime}\n출차시간 : {car.outTime}");
+    }
+}
---
+++ RandomClass/ListClassTest/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random r = new Random();
+        List<int> list = new List<int>();
+        Console.Write("입력 받을 랜덤 수의 개수를 입력하세요. :  ");
+        int num = int.Parse(Console.ReadLine());
+        for (int i = 0; i < num; i++)
+        {
+            list.Add(r.Next(1, 100));
+        }
+
+        // 리스트 원소 개수와 랜덤 수 출력 (list.Count 이용)
+        Console.WriteLine("Count : " + list.Count);
+        Console.Write("list : ");
+        foreach (var item in list)
+        {
+            Console.Write("{0, 3}", item);
+        }
+
+        // 특정 위치의 항목 제거 (list.RemoveAt 이용)
+        Console.WriteLine("\n\n------- 항목 삭제 후 1 -------");
+        Console.WriteLine("list.RemoveAt(0) -> 0번지 인덱스 항목 삭제");
+        list.RemoveAt(0);
+        Console.WriteLine("Count : " + list.Count);
+        Console.Write("list : ");
+        foreach (var item in list)
+        {
+            Console.Write("{0, 3}", item);
+        }
+
+        // 마지막만 뺴고 다 지운 후 출력 (list.RemoveRange 이용)
+        Console.WriteLine("\n\n------- 항목 삭제 후 2 -------");
+        Console.WriteLine("list.RemoveRange(0, list.Count - 1) -> 0번지부터 마지막 전까지의 항목 삭제");
+        list.RemoveRange(0, list.Count - 1);
+        Console.WriteLine("Count : " + list.Count);
+        Console.Write("list : ");
+        foreach (var item in list)
+        {
+            Console.Write("{0, 3}", item);
+        }
+    }
+}
