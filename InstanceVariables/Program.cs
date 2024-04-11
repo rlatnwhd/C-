d93c2451257bda72c97d116f96d729946c1c15ee
김수종 <127273652+rlatnwhd@users.noreply.github.com>
2024-04-11 14:10:05 +0900

    2024-04-11

---
+++ InstanceVariables/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    class Product
+    {
+        public string name;
+        public int price;
+        public void Print()
+        {
+            Console.WriteLine($"name : {name}\nprice : {price}");
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Product productA = new Product() { name = "감자", price = 2000 };
+        Product productB = new Product() { name = "고구마", price = 3000 };
+        productA.Print();
+        productB.Print();
+    }
+}
