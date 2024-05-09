1f4857ba561e5a40faffae1a40e8f519a3d68e06
Kim sujong <127273652+rlatnwhd@users.noreply.github.com>
2024-05-09 14:19:49 +0900

    2024-05-09

---
+++ Blnheritance/Program.cs
@@ -0,0 +1,57 @@
+﻿class Animal
+{
+    public int Age { get; set; }
+    public Animal() { Age = 0; }
+
+    public void Eat() { Console.WriteLine("냠냠"); }
+    public void Sleep() { Console.WriteLine("쿨쿨"); }
+}
+
+class Dog : Animal
+{
+    public string Color { get; set; }
+
+    public void Bark() { Console.WriteLine("멍멍"); }
+}
+
+class Cat : Animal
+{
+    public void Meow() { Console.WriteLine("냥냥"); }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // List<Dog> dogs = new List<Dog>() { new Dog(), new Dog(), new Dog() };
+        // List<Cat> cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
+        List<Animal> animals = new List<Animal>()
+        {
+            new Dog(), new Dog(),
+            new Cat(), new Cat()
+        };
+
+        foreach (var item in animals)
+        {
+            item.Eat();
+            item.Sleep();
+
+            if (item is Dog) { ((Dog)item).Bark(); }
+            if (item is Cat) { ((Cat)item).Meow(); }
+        }
+
+        // foreach (var item in dogs)
+        // {
+        //     item.Eat();
+        //     item.Sleep();
+        //     item.Bark();
+        // }
+         
+        // foreach (var item in cats)
+        // {
+        //     item.Eat();
+        //     item.Sleep();
+        //     item.Meow();
+        // }
+    }
+}
---
+++ ExtendsTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Channels;
+
+class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public void set(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void showPoint()
+    {
+        Console.WriteLine("(" + X + "," + Y + ")");
+    }
+}
+class ColorPoint : Point
+{
+    public string Color { get; set; }
+
+    public void setColor(string color)
+    {
+        Color = color;
+    }
+
+    public void showColorPoint()
+    {
+        Console.Write(Color);
+        showPoint();
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Point p = new Point();
+        p.set(1, 2);
+        p.showPoint();
+
+        ColorPoint cp = new ColorPoint();
+        cp.set(3, 4);
+        cp.setColor("red");
+        cp.showColorPoint();
+    }
+}
