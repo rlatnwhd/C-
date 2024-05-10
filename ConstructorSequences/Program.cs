5d184001c40d755e2b966cf52cdbb1a50c8595be
Kim sujong <127273652+rlatnwhd@users.noreply.github.com>
2024-05-10 10:08:24 +0900

    2024-05-10

---
+++ ConstructorSequences/Program.cs
@@ -0,0 +1,30 @@
+﻿class Parent
+{
+    public Parent() { Console.WriteLine("Parent()"); }
+    public Parent(int param) { Console.WriteLine("Parent(int param)"); }
+    public Parent(string param) { Console.WriteLine("Parent(string param)"); }
+}
+class Child : Parent
+{
+    public Child() : base()
+    {
+        Console.WriteLine("Child() : base()\n");
+    }
+    public Child(int num) : base(num)
+    {
+        Console.WriteLine("Child() : base(num)\n");
+    }
+    public Child(string input) : base(input)
+    {
+        Console.WriteLine("Child(string input) : base(input)\n");
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Child childA = new Child();
+        Child childB = new Child(10);
+        Child childC = new Child("string");
+    }
+}
