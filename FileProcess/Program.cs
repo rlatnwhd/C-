9d23ee4905441587c65cfc501594faaab7087e3e
Kim sujong <127273652+rlatnwhd@users.noreply.github.com>
2024-05-24 11:12:11 +0900

    2024-05-24

---
+++ FileProcess/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using (StreamWriter sw = new StreamWriter(@"c:\test\test.txt"))
+        {
+            sw.WriteLine("안녕하세요");
+            sw.WriteLine("StreamWriter 클래스를 사용해");
+            sw.WriteLine("글자를 여러 줄 입력해봅시다.");
+
+            for (int i = 0; i < 10; i++)
+            {
+                sw.WriteLine("반복문 - " + i);
+            }
+
+        }
+
+        using (StreamReader sr = new StreamReader(@"c:\test\test.txt"))
+        {
+            string line;
+            while (true)
+            {
+                line = sr.ReadLine();
+                if (Equals(line, "반복문 - 0"))
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                    break;
+                }
+            }
+        }
+            
+        
+    }
+}
---
+++ InterfaceTest/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace InterfaseTest
+{
+    internal class Program
+    {
+        interface IUpgradable
+        {
+            void Upgrade();
+        }
+        interface ISellable
+        {
+            void Sell();
+        }
+        class Monster : IUpgradable, ISellable
+        {
+            public int Level { get; set; }
+            public int Price { get; set; }
+
+            public void Sell()
+            {
+                Console.WriteLine("Sell Monster : $" + Price);
+            }
+
+            public void Upgrade()
+            {
+                Price += 1000;
+                Console.WriteLine("Upgrade Monster : Level " + ++Level);
+            }
+        }
+
+        class Item : ISellable
+        {
+            public int Price { get; set; }
+
+            public void Sell()
+            {
+                Console.WriteLine("Sell Item : $" + Price);
+            }
+        }
+        private static void Main(string[] args)
+        {
+            Monster monster = new Monster();
+            monster.Level = 3;
+            monster.Price = 2000;
+
+            monster.Upgrade();
+            monster.Sell();
+
+            Item item = new Item();
+            item.Price = 500;
+            item.Sell();
+        }
+    }
+}
